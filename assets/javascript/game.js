10a94f86cf84eef9fec9fe73f5f3e68afee5399a
ashdonnelly <ashdonnelly>
2017-10-03 00:15:08 -0600

    do ok code with alerts not doc write

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,87 @@
-var word = ["spell", "coven", "raven", "cat",
- "familiar", "devil", "magic", "cauldron", "potion", "curse",
- "demon", "hex", "ancient", "occult", "illusion", "moonlight",
- "mystic", "necromancer", "sacrificial", "shaman", "shapeshifter",
- "bonfire", "enchantment", "conjure", "alchemy", "mystic", "siren",
- "sorceress", "trance", "charm", "rune", "wizardry", "conjuration"
- "oracle", "omen", "supernatural", "ritual", "snake", "incubus",
- "succubus", "sleep paralysis", "nightmare", "serpent", "graveyard",
- "tomb", "flames", "poison"]
+var words = [
+	"spell",
+	"coven",
+	"raven",
+	"familiar",
+	"devil",
+	"magic",
+	"cauldron",
+	"potion",
+	"curse",
+	"demon",
+	"hex",
+	"ancient",
+	"occult",
+	"illusion",
+	"moonlight",
+	"mystic",
+	"necromancer",
+	"sacrifice",
+	"shaman",
+	"shapeshift",
+	"bonfire",
+	"enchantment",
+	"conjure",
+	"alchemy",
+	"mystic",
+	"siren",
+	"sorceress",
+	"trance",
+	"charm",
+	"rune",
+	"wizardry",
+	"hermit",
+	"oracle",
+	"omen",
+	"supernatural",
+	"ritual",
+	"snake",
+	"incubus",
+	"succubus",
+	"nightmare",
+	"serpent",
+	"cemetary",
+	"tomb",
+	"flames",
+	"spirit",
+	"poison",
+	"venom",
+	"spellbook",
+	"crone",
+	"chant",
+	"shadows"
+];
 
+var word = words[Math.floor(Math.random() * words.length)];
 
+var answerArray = [];
 
+for (var i = 0; i < word.length; i++) {
+	answerArray[i] = "_";
+}
 
- if (word ) {
+var remainingLetters = word.length;
 
- } else {
+while (remainingLetters > 0) {
 
- }
+	alert(answerArray.join(" "));
+
+	var guess = prompt("Guess a letter, or click cancel to stop playing.");
+
+	if (guess === null) {
+		break;
+	} else if (guess.length !== 1) {
+		alert("Please enter a single letter.");
+	} else {
+		for (var j = 0; j < word.length; j++) {
+			if (word[j] === guess) {
+				answerArray[j] = guess;
+				remainingLetters--;
+			}
+		}
+	}
+}
+
+alert(answerArray.join(" "));
+
+alert("Good job! The answer was " + word + ".");
