d064470a827b3b4fd2869e250ad52df7a74ad37f
ashdonnelly <ashdonnelly>
2017-10-02 19:05:50 -0600

    do most html and css; start javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+var word = ["spell", "coven", "raven", "cat",
+ "familiar", "devil", "magic", "cauldron", "potion", "curse",
+ "demon", "hex", "ancient", "occult", "illusion", "moonlight",
+ "mystic", "necromancer", "sacrificial", "shaman", "shapeshifter",
+ "bonfire", "enchantment", "conjure", "alchemy", "mystic", "siren",
+ "sorceress", "trance", "charm", "rune", "wizardry", "conjuration"
+ "oracle", "omen", "supernatural", "ritual", "snake", "incubus",
+ "succubus", "sleep paralysis", "nightmare", "serpent", "graveyard",
+ "tomb", "flames", "poison"]
+
+
+
+
+ if (word ) {
+
+ } else {
+
+ }
